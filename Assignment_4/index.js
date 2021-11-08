567fdb63cb3c48fdc4f407673ba992e5a046afca
SBL15 <lokhandesuraj807@gmail.com>
2021-11-08 16:51:00 +0530

    update

--- Assignment_4/index.js
+++ Assignment_4/index.js
@@ -1,20 +1,20 @@
 var names = new Array();
-names[0]="Shami";
+names[0]="Yaakov";
 names[1]="John";
-names[2]="Sunny";
-names[3]="Vicky";
-names[4]="Johny";
-names[5]="Danie";
-names[6]="Bhuvan";
-names[7]="Janardhan";
-names[8]="Lucky";
-names[9]="Joe";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
 
 for (var i = 0; i < names.length; i++)
  {
 	if (names[i].charAt(0)==="J" || names[i].charAt(0)==="j") 
 	{
-		console.log("Goodbye " +names[i])
+		console.log("Good Bye " +names[i])
 	}
 	else
 	{
